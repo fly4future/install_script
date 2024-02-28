b4a723c311c8c2ee5c1c70eee575830bd8c26f6c
Dan Hert <hertdani@fel.cvut.cz>
2024-02-28 12:30:36 +0100

    fixes

--- subscripts/1Install/4Install_Klaxalk_linux-setup_(MRS_internal_Only).sh
+++ subscripts/1Install/4Install_Klaxalk_linux-setup_(MRS_internal_Only).sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p ~/git
-git clone https://github.com/klaxalk/linux-setup.git ~/git/
+git clone https://github.com/klaxalk/linux-setup.git ~/git/linux-setup
 ~/git/linux-setup/install.sh
 
 exit 0
--- subscripts/2PostInstall/1Setup_ROS_workspace.sh
+++ subscripts/2PostInstall/1Setup_ROS_workspace.sh
@@ -28,6 +28,16 @@ if [[ "$ret_val" -eq 255 ]]; then
   exit 1
 fi
 
+echo "Setting up ~/workspace..."
+source /opt/ros/noetic/setup.bash             # source the general ROS workspace so that the local one will extend it and see all the packages
+mkdir -p ~/workspace/src && cd ~/workspace    # create the workspace folder in home and cd to it
+catkin init -w ~/workspace                    # initialize the new workspace
+# setup basic compilation profiles
+catkin config --profile debug --cmake-args -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS='-std=c++17 -Og' -DCMAKE_C_FLAGS='-Og'
+catkin config --profile release --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS='-std=c++17'
+catkin config --profile reldeb --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS='-std=c++17'
+catkin profile set reldeb                     # set the reldeb profile as active
+
 mkdir ~/git
 cd ~/git
 for CHOICE in $SELECTIONS; do
@@ -46,17 +56,6 @@ for CHOICE in $SELECTIONS; do
       ;;
   esac
 done
-
-echo "Setting up ~/workspace..."
-source /opt/ros/noetic/setup.bash             # source the general ROS workspace so that the local one will extend it and see all the packages
-mkdir -p ~/workspace/src && cd ~/workspace    # create the workspace folder in home and cd to it
-catkin init -w ~/workspace                    # initialize the new workspace
-# setup basic compilation profiles
-catkin config --profile debug --cmake-args -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS='-std=c++17 -Og' -DCMAKE_C_FLAGS='-Og'
-catkin config --profile release --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS='-std=c++17'
-catkin config --profile reldeb --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS='-std=c++17'
-catkin profile set reldeb                     # set the reldeb profile as active
-
 
 cd ~/workspace
 catkin build
