2af7904e872736b5ed9da33cffdf2f576025b26e
Dan Hert <hertdani@fel.cvut.cz>
2024-04-02 09:38:09 +0200

    up

--- setup_utility.sh
+++ setup_utility.sh
@@ -95,8 +95,8 @@ if [ -z "$1" ]
 then
   folder_path="$DIR/subscripts"
   sudo chmod -R +x "$DIR/subscripts"
-  sudo chmod -R -x "$DIR/subscripts/5Udev_rules/DISREGARD_udev_rules"
-  sudo chmod +x "$DIR/subscripts/5Udev_rules/DISREGARD_udev_rules"
+  sudo chmod -R -x "$DIR/subscripts/7Udev_rules/DISREGARD_udev_rules"
+  sudo chmod +x "$DIR/subscripts/7Udev_rules/DISREGARD_udev_rules"
 else
   folder_path="$1"
   first_run=false
