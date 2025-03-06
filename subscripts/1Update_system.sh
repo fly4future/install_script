cc3256529d7d735d5fcde2bbd9517bf52cea1443
Dan Hert <hertdani@fel.cvut.cz>
2025-03-06 12:07:38 +0100

    Added WiFi driver reinstall and hold to the update script

--- subscripts/1Update_system.sh
+++ subscripts/1Update_system.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sudo apt -y update && rosdep update && sudo apt -y upgrade --with-new-pkgs --allow-downgrades
+sudo apt install linux-modules-iwlwifi-$(uname -r) #Install correct wi-fi drivers - problem on new machines with ubuntu 20j
+sudo apt-mark hold linux-modules-iwlwifi-$(uname -r) #Hold the wifi driver at this version
 source ~/.bashrc
 exit 0
 
