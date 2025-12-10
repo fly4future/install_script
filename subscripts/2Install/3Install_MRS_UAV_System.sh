b9eda63eb5275badc2f2da46d11a786b892bb7ba
Daniel Hert <hert@fly4future.com>
2025-12-10 14:31:29 +0100

    adding update after system install

--- subscripts/2Install/3Install_MRS_UAV_System.sh
+++ subscripts/2Install/3Install_MRS_UAV_System.sh
@@ -59,4 +59,8 @@ link_tmux() {
 
 main
 link_tmux
+
+sudo apt -y update && rosdep update && sudo apt -y upgrade --with-new-pkgs --allow-downgrades
+sudo apt install linux-modules-iwlwifi-$(uname -r) #Install correct wi-fi drivers - problem on new machines with ubuntu 20j
+sudo apt-mark hold linux-modules-iwlwifi-$(uname -r) #Hold the wifi driver at this version
 exit 0
