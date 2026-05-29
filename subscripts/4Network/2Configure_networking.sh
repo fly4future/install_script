27b72615b7a028969c3bf3f8feb4b6917649da02
Vid Smole <smole@fly4future.com>
2026-05-29 14:53:23 +0200

    Disable network manager immediately when configuring networking

--- subscripts/4Network/2Configure_networking.sh
+++ subscripts/4Network/2Configure_networking.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yesno_def_no () {
-  whiptail --title "Netplan Config" --yesno "$1" --yes-button "No" --no-button "Yes" 0 0
+  whiptail --title "Network Config" --yesno "$1" --yes-button "No" --no-button "Yes" 0 0
   ret_val=$?
 
   if [ $ret_val -eq 255 ]; then
@@ -18,7 +18,7 @@ yesno_def_no () {
 }
 
 yesno_def_yes () {
-  whiptail --title "Netplan Config" --yesno "$1"  0 0
+  whiptail --title "Network Config" --yesno "$1"  0 0
   ret_val=$?
 
   if [ $ret_val -eq 255 ]; then
@@ -55,10 +55,30 @@ input_box () {
 }
 
 error_msg () {
-  whiptail --title "Netplan config" --msgbox "$1" 0 0
+  whiptail --title "Network config" --msgbox "$1" 0 0
 }
 
-yesno_def_yes "Delete any previous netplan configs? (Recommended)"
+enable_systemd_networkd () {
+  sudo systemctl unmask systemd-networkd systemd-resolved
+  sudo systemctl enable --now systemd-networkd systemd-resolved
+}
+
+disable_network_manager () {
+  sudo systemctl disable --now NetworkManager NetworkManager-wait-online NetworkManager-dispatcher
+  sudo systemctl mask NetworkManager NetworkManager-wait-online NetworkManager-dispatcher
+}
+
+yesno_def_yes "This script will configure networking on the device. Systemd-networkd will be used as the backend for netplan and NetworkManager will be disabled. If you're connected via SSH you may lose connection when applying. Do you want to continue?"
+ret_val=$?
+
+if [ $ret_val -eq 1 ]; then
+  echo "Continuing with network config..."
+elif [ $ret_val -eq 0 ]; then
+  echo "Aborting network config..."
+  exit 0
+fi
+
+yesno_def_yes "Delete all previous netplan configs? (Recommended)"
 ret_val=$?
 
 if [ $ret_val -eq 1 ]; then
@@ -74,7 +94,6 @@ echo "  version: 2" >> /tmp/01-netcfg.yaml
 echo "  renderer: networkd" >> /tmp/01-netcfg.yaml
 
 interfaces=$(ls /sys/class/net)
-# interfaces="eth0 eth1 wlan0 wlan1"
 eths=$(echo $interfaces | grep -o "\w*eth\w*")
 wlans=$(echo $interfaces | grep -o "\w*wlan\w*")
 
@@ -83,10 +102,10 @@ if [ -z "${eths}" ]; then
   error_msg "No Ethernet interfaces found! (looking for eth0, eth1 ...).\nYour Ethernet interfaces may have different names, run the Network Interface Names Fix first.\n\n\n Continuing with Wi-Fi config. "
 else
   echo "  ethernets:" >> /tmp/01-netcfg.yaml
-  for name in ${eths}; do
-    echo "    $name:" >> /tmp/01-netcfg.yaml
+  for int in ${eths}; do
+    echo "    $int:" >> /tmp/01-netcfg.yaml
 
-    yesno_def_no "Do you want to use DHCP on $name?"
+    yesno_def_no "Do you want to use DHCP on $int?"
     ret_val=$?
 
     if [ $ret_val -eq 1 ]; then
@@ -96,7 +115,7 @@ else
       echo "      dhcp4: no" >> /tmp/01-netcfg.yaml
       echo "      dhcp6: no" >> /tmp/01-netcfg.yaml
 
-      address=$(input_box "Enter your static IP address:" "10.10.20.101")
+      address=$(input_box "Enter your static IP address for $int:" "10.10.20.101")
       echo "      addresses: [$address/24]" >> /tmp/01-netcfg.yaml
 
     fi
@@ -109,10 +128,10 @@ if [ -z "${wlans}" ]; then
 else
   echo "  wifis:" >> /tmp/01-netcfg.yaml
 
-  for name in ${wlans}; do
-    echo "    $name:" >> /tmp/01-netcfg.yaml
+  for int in ${wlans}; do
+    echo "    $int:" >> /tmp/01-netcfg.yaml
 
-    yesno_def_no "Do you want to use DHCP on $name?"
+    yesno_def_no "Do you want to use DHCP on $int?"
     dhcp=$?
 
     if [ $dhcp -eq 1 ]; then
@@ -124,17 +143,17 @@ else
 
       address=""
       if [ $1 = "f4f" ]; then
-        address=$(input_box "Enter your static IP address:" "192.168.12.101")
+        address=$(input_box "Enter your static IP address for $int:" "192.168.12.101")
       else 
-        address=$(input_box "Enter your static IP address:" "192.168.69.101")
+        address=$(input_box "Enter your static IP address for $int:" "192.168.69.101")
       fi
       echo "      addresses: [$address/24]" >> /tmp/01-netcfg.yaml
 
       gateway=""
       if [ $1 = "f4f" ]; then
-        gateway=$(input_box "Enter your gateway address:" "192.168.12.1")
+        gateway=$(input_box "Enter your default gateway address:" "192.168.12.1")
       else 
-        gateway=$(input_box "Enter your gateway address:" "192.168.69.1")
+        gateway=$(input_box "Enter your default gateway address:" "192.168.69.1")
       fi
       echo "      gateway4: $gateway" >> /tmp/01-netcfg.yaml
 
@@ -142,15 +161,15 @@ else
 
     ap_name=""
     if [ $1 = "f4f" ]; then
-      ap_name=$(input_box "Enter your access point name:" "f4f_robot")
+      ap_name=$(input_box "Enter your WiFi name (SSID):" "f4f_robot")
     else 
-      ap_name=$(input_box "Enter your access point name:" "mrs_ctu")
+      ap_name=$(input_box "Enter your WiFi name (SSID):" "mrs_ctu")
     fi
 
     echo "      access-points:" >> /tmp/01-netcfg.yaml
     echo "        \"$ap_name\":" >> /tmp/01-netcfg.yaml
 
-    password=$(input_box "Enter your access point password:" "mikrokopter")
+    password=$(input_box "Enter your WiFi password:" "mikrokopter")
     echo "          password: \"$password\"" >> /tmp/01-netcfg.yaml
 
     if [ $dhcp -eq 0 ]; then
@@ -163,14 +182,26 @@ else
 fi
 
 netplan=$(cat /tmp/01-netcfg.yaml);
-yesno_def_yes "This netplan was generated: \n\n $netplan \n\n Copy to /etc/netplan and Apply?"
+yesno_def_yes "The following netplan was generated: \n\n $netplan \n\n Copy to /etc/netplan and Apply?"
 ret_val=$?
 
 if [ $ret_val -eq 1 ]; then
+
+  # First ensure systemd-networkd is enabled and running
+  echo "Enabling systemd-networkd ..."
+  enable_systemd_networkd
+
+  # Then apply the netplan config
   echo "Copying netplan ..."
   sudo cp /tmp/01-netcfg.yaml /etc/netplan
   echo "Applying netplan ..."
+  sudo netplan generate
   sudo netplan apply
+
+  # Finally, disable network manager
+  echo "Disabling NetworkManager ..."
+  disable_network_manager
+
   echo "Done"
   exit 0
 fi
