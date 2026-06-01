5b2b5eeeb7fe1df9892749eb2ecae3d902ae2e91
Vid Smole <smole@fly4future.com>
2026-06-01 12:10:02 +0200

    Add support for disabling "Predictable Network Interface Names" on non-grub devices
    Jetsons are using extlinux instead of grub for booting, so the way to pass parameters for disabling predictable network interface names is different.
    
    Note that OS directly from nvidia already has this disabled but the Seeed firmware has it enabled and this script has to be used.

--- subscripts/4Network/1Fix_network_interface_names.sh
+++ subscripts/4Network/1Fix_network_interface_names.sh
@@ -1,7 +1,22 @@
-#!/bin/bash 
+#!/bin/bash
 
-sudo sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
-sudo update-grub
+set -e
+
+if [[ -f /etc/default/grub ]]; then
+    # Device is using GRUB (e.g. Intel NUC)
+    sudo sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
+    sudo update-grub
+elif [[ -f /boot/extlinux/extlinux.conf ]]; then
+    # Device is using extlinux (e.g. NVIDIA Jetson)
+	if ! grep -q 'net.ifnames=0' /boot/extlinux/extlinux.conf; then
+		sudo cp /boot/extlinux/extlinux.conf /boot/extlinux/extlinux.conf.bak
+		sudo sed -i '/^[[:space:]]*APPEND / s/$/ net.ifnames=0 biosdevname=0/' /boot/extlinux/extlinux.conf
+	fi
+else
+	echo "Could not find a supported bootloader config."
+	echo "Expected /etc/default/grub or /boot/extlinux/extlinux.conf"
+	exit 1
+fi
 
 echo " "
 echo "Changes will be applied only after system reboot"
