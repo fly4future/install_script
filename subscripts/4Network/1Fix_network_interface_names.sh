994854fcca203f4969b9354b022b8df7c476693b
Vid Smole <smole@fly4future.com>
2026-06-01 12:22:12 +0200

    Override if already present

--- subscripts/4Network/1Fix_network_interface_names.sh
+++ subscripts/4Network/1Fix_network_interface_names.sh
@@ -8,9 +8,16 @@ if [[ -f /etc/default/grub ]]; then
     sudo update-grub
 elif [[ -f /boot/extlinux/extlinux.conf ]]; then
     # Device is using extlinux (e.g. NVIDIA Jetson)
-	if ! grep -q 'net.ifnames=0' /boot/extlinux/extlinux.conf; then
-		sudo cp /boot/extlinux/extlinux.conf /boot/extlinux/extlinux.conf.bak
-		sudo sed -i '/^[[:space:]]*APPEND / s/$/ net.ifnames=0 biosdevname=0/' /boot/extlinux/extlinux.conf
+	sudo cp /boot/extlinux/extlinux.conf /boot/extlinux/extlinux.conf.bak
+	if grep -q 'net.ifnames=' /boot/extlinux/extlinux.conf; then
+		sudo sed -i 's/net.ifnames=[^[:space:]]*/net.ifnames=0/g' /boot/extlinux/extlinux.conf
+	else
+		sudo sed -i '/^[[:space:]]*APPEND / s/$/ net.ifnames=0/' /boot/extlinux/extlinux.conf
+	fi
+	if grep -q 'biosdevname=' /boot/extlinux/extlinux.conf; then
+		sudo sed -i 's/biosdevname=[^[:space:]]*/biosdevname=0/g' /boot/extlinux/extlinux.conf
+	else
+		sudo sed -i '/^[[:space:]]*APPEND / s/$/ biosdevname=0/' /boot/extlinux/extlinux.conf
 	fi
 else
 	echo "Could not find a supported bootloader config."
