e2c370b8cbca3c53085e2624a813003a5c87f917
Vid Smole <smole@fly4future.com>
2026-06-09 16:25:05 +0200

    Fix wifi config

--- subscripts/4Network/2Configure_networking.sh
+++ subscripts/4Network/2Configure_networking.sh
@@ -166,7 +166,7 @@ if [[ "$NON_INTERACTIVE_MODE" -ne 1 ]]; then
         echo "      dhcp6: no" >>/tmp/01-netcfg.yaml
 
         address=""
-        if [[ "$1" == "f4f" ]]; then
+      if [[ $USE_DEFAULTS_FOR == "F4F" ]]; then
           address=$(input_box "Enter your static IP address for $int:" "192.168.12.101")
         else
           address=$(input_box "Enter your static IP address for $int:" "192.168.69.101")
@@ -174,7 +174,7 @@ if [[ "$NON_INTERACTIVE_MODE" -ne 1 ]]; then
         echo "      addresses: [$address/24]" >>/tmp/01-netcfg.yaml
 
         gateway=""
-        if [[ "$1" == "f4f" ]]; then
+      if [[ $USE_DEFAULTS_FOR == "F4F" ]]; then
           gateway=$(input_box "Enter your default gateway address:" "192.168.12.1")
         else
           gateway=$(input_box "Enter your default gateway address:" "192.168.69.1")
@@ -185,16 +185,6 @@ if [[ "$NON_INTERACTIVE_MODE" -ne 1 ]]; then
         echo "          metric: 200" >>/tmp/01-netcfg.yaml
 
       fi
-
-      ap_name=""
-      if [[ "$1" == "f4f" ]]; then
-        ap_name=$(input_box "Enter your WiFi name (SSID):" "f4f_robot")
-      else
-        ap_name=$(input_box "Enter your WiFi name (SSID):" "mrs_ctu")
-      fi
-
-      echo "      access-points:" >>/tmp/01-netcfg.yaml
-      echo "        \"$ap_name\":" >>/tmp/01-netcfg.yaml
 
       ap_name=""
       if [[ $USE_DEFAULTS_FOR == "F4F" ]]; then
