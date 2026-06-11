0abc12eff7fddfca200360e5cc17919935144e0c
Vid Smole <smole@fly4future.com>
2026-06-11 09:45:47 +0200

    Install nvidia container toolkit when installing docker
    Only when a jetson device is detected, otherwise it is the same as before

--- subscripts/2Install/6Install_Docker.sh
+++ subscripts/2Install/6Install_Docker.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-# https://docs.docker.com/engine/install/ubuntu/
+# Check if we're running on an NVIDIA Jetson device.
+# In that case we have to also install the NVIDIA Container Toolkit, 
+# which comprises of a few additional steps compared to a normal docker installation (https://docs.docker.com/engine/install/ubuntu/)
+# See https://docs.nvidia.com/jetson/agx-thor-devkit/user-guide/latest/setup_docker.html
+grep -q "NVIDIA Jetson" /proc/device-tree/model > /dev/null 2>&1
+is_jetson=$?
+
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+
+if [ "$is_jetson" -eq 0 ]; then
+    sudo apt-get install -y nvidia-container jq
+fi
 
 # Add Docker's official GPG key:
-sudo apt update
-sudo apt install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -24,4 +34,19 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin d
 sudo usermod -aG docker "$USER"
 
 sudo systemctl enable --now docker
+
+if [ "$is_jetson" -eq 0 ]; then
+    sudo nvidia-ctk runtime configure --runtime=docker
+    sudo systemctl daemon-reload 
+    sudo systemctl restart docker
+
+    sudo jq '. + {"default-runtime": "nvidia"}' /etc/docker/daemon.json | \
+    sudo tee /etc/docker/daemon.json.tmp && \
+    sudo mv /etc/docker/daemon.json.tmp /etc/docker/daemon.json
+fi
+
+echo
+echo "Docker installation complete. Please log out and log back in to apply the changes to your user group and be able to use the docker command without sudo."
+echo
+
 exit 0
