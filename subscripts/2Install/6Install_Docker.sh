9f00970a9fd284d24746aeeaa459f47e4d1f5c87
Vid Smole <smole@fly4future.com>
2026-06-12 13:52:03 +0200

    Use correct package

--- subscripts/2Install/6Install_Docker.sh
+++ subscripts/2Install/6Install_Docker.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-# Check if we're running on an NVIDIA Jetson device.
-# In that case we have to also install the NVIDIA Container Toolkit, 
-# which comprises of a few additional steps compared to a normal docker installation (https://docs.docker.com/engine/install/ubuntu/)
-# See https://docs.nvidia.com/jetson/agx-thor-devkit/user-guide/latest/setup_docker.html
-grep -q "NVIDIA Jetson" /proc/device-tree/model > /dev/null 2>&1
-is_jetson=$?
-
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl
 
-if [ "$is_jetson" -eq 0 ]; then
-    sudo apt-get install -y nvidia-container jq
+# Check if we're running on an NVIDIA Jetson device.
+if grep -q "NVIDIA Jetson" /proc/device-tree/model > /dev/null 2>&1; then
+    sudo apt-get install -y nvidia-container-toolkit jq
 fi
 
 # Add Docker's official GPG key:
@@ -31,22 +25,24 @@ EOF
 
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo usermod -aG docker "$USER"
-
 sudo systemctl enable --now docker
 
-if [ "$is_jetson" -eq 0 ]; then
+if grep -q "NVIDIA Jetson" /proc/device-tree/model > /dev/null 2>&1; then
+    echo "Configuring Docker to use NVIDIA Container Toolkit as the default runtime..."
     sudo nvidia-ctk runtime configure --runtime=docker
-    sudo systemctl daemon-reload 
+    sudo systemctl daemon-reload
     sudo systemctl restart docker
 
-    sudo jq '. + {"default-runtime": "nvidia"}' /etc/docker/daemon.json | \
-    sudo tee /etc/docker/daemon.json.tmp && \
+    sudo jq '. + {"default-runtime": "nvidia"}' /etc/docker/daemon.json | sudo tee /etc/docker/daemon.json.tmp
     sudo mv /etc/docker/daemon.json.tmp /etc/docker/daemon.json
+    sudo systemctl restart docker
 fi
 
-echo
-echo "Docker installation complete. Please log out and log back in to apply the changes to your user group and be able to use the docker command without sudo."
-echo
+echo "Add user $USER to docker group"
+sudo usermod -aG docker $USER
+
+echo ""
+echo "Docker installation complete. Please log out and log back in to apply the changes to your group and be able to use the docker command without sudo."
+echo ""
 
 exit 0
