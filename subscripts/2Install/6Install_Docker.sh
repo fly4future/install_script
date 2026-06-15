9bb12a6f73d78a22081ae9ab67825d22300127ff
Vid Smole <smole@fly4future.com>
2026-06-15 07:42:55 +0200

    feat: Install nvidia container toolkit when installing docker (#18)
    * Install nvidia container toolkit when installing docker

--- subscripts/2Install/6Install_Docker.sh
+++ subscripts/2Install/6Install_Docker.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-# https://docs.docker.com/engine/install/ubuntu/
+# Instructions for installing Docker: https://docs.docker.com/engine/install/ubuntu
+# Instructions for installing and configuring Nvidia Container Toolkit: https://docs.nvidia.com/jetson/orin-nano-devkit/user-guide/latest/setup_docker.html
+# Note that the instructions for the toolkit are wrong. The correct package to install is nvidia-container-toolkit, not nvidia-container, see https://github.com/fly4future/install_script/pull/18 for more info.
+
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+
+# Check if we're running on an NVIDIA Jetson device.
+if grep -q "NVIDIA Jetson" /proc/device-tree/model > /dev/null 2>&1; then
+    sudo apt-get install -y nvidia-container-toolkit jq
+fi
 
 # Add Docker's official GPG key:
-sudo apt update
-sudo apt install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -21,7 +29,24 @@ EOF
 
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo usermod -aG docker "$USER"
-
 sudo systemctl enable --now docker
+
+if grep -q "NVIDIA Jetson" /proc/device-tree/model > /dev/null 2>&1; then
+    echo "Configuring Docker to use NVIDIA Container Toolkit as the default runtime..."
+    sudo nvidia-ctk runtime configure --runtime=docker
+    sudo systemctl daemon-reload
+    sudo systemctl restart docker
+
+    sudo jq '. + {"default-runtime": "nvidia"}' /etc/docker/daemon.json | sudo tee /etc/docker/daemon.json.tmp
+    sudo mv /etc/docker/daemon.json.tmp /etc/docker/daemon.json
+    sudo systemctl restart docker
+fi
+
+echo "Add user $USER to docker group"
+sudo usermod -aG docker $USER
+
+echo ""
+echo "Docker installation complete. Please log out and log back in to apply the changes to your group and be able to use the docker command without sudo."
+echo ""
+
 exit 0
