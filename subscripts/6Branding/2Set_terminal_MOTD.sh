194da433c9bfe3bfe413b24a7e96bf23d91b6aee
Vid Smole <smole@fly4future.com>
2026-06-29 10:41:08 +0200

    Switch to remove execute permission and remove unminimizing message

--- subscripts/6Branding/2Set_terminal_MOTD.sh
+++ subscripts/6Branding/2Set_terminal_MOTD.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo rm -rf /etc/update-motd.d/10-help-text
-sudo rm -rf /etc/update-motd.d/90-updates-available
+sudo chmod -x /etc/update-motd.d/10-help-text
+sudo chmod -x /etc/update-motd.d/60-unminimize
+sudo chmod -x /etc/update-motd.d/90-updates-available
 
 sudo cp subscripts/6Branding/DISREGARD/10-fly4future /etc/update-motd.d/10-fly4future
 sudo chmod +x /etc/update-motd.d/10-fly4future
