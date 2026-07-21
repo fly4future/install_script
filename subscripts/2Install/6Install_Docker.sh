8d1233778ebadb461c9faf1836cb6c68a61369a7
Vid Smole <smole@fly4future.com>
2026-07-21 16:06:47 +0200

    Do not run update, we already run it at the start of TUI

--- subscripts/2Install/6Install_Docker.sh
+++ subscripts/2Install/6Install_Docker.sh
@@ -4,7 +4,8 @@
 # Instructions for installing and configuring Nvidia Container Toolkit: https://docs.nvidia.com/jetson/orin-nano-devkit/user-guide/latest/setup_docker.html
 # Note that the instructions for the toolkit are wrong. The correct package to install is nvidia-container-toolkit, not nvidia-container, see https://github.com/fly4future/install_script/pull/18 for more info.
 
-sudo apt-get update
+set -e
+
 sudo apt-get install -y ca-certificates curl
 
 # Check if we're running on an NVIDIA Jetson device.
