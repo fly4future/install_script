0f5238a952ba05859561ebaa5447f202cd5bbea8
Vid Smole <smole@fly4future.com>
2026-08-04 11:01:12 +0200

    Fix setting profile picture

--- subscripts/6Branding/2Set_profile_picture.sh
+++ subscripts/6Branding/2Set_profile_picture.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-sudo cp subscripts/6Branding/DISREGARD/profile.png /home/$USER/.face
+destination="/var/lib/AccountsService/icons/$USER"
+
+sudo cp subscripts/6Branding/DISREGARD/profile.png "$destination"
+
+# Ensure the profile picture for GNOME is pointed to the correct location
+sudo sed -i "s|^Icon=.*$|Icon=$destination|" "/var/lib/AccountsService/users/$USER"
 
 echo "Profile picture has been set."
 
