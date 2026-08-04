39f876e6d787907f965bcf0df773ecddac93cfbf
Vid Smole <smole@fly4future.com>
2026-08-04 10:26:17 +0200

    Change linux-setup to branch 24.04 (#32)

--- subscripts/2Install/4Install_Klaxalk_linux-setup_(MRS_internal_Only).sh
+++ subscripts/2Install/4Install_Klaxalk_linux-setup_(MRS_internal_Only).sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p ~/git
-git clone https://github.com/klaxalk/linux-setup.git ~/git/linux-setup
+git clone https://github.com/klaxalk/linux-setup.git -b 24.04 ~/git/linux-setup
 bash ~/git/linux-setup/install.sh --unattended
 
 exit 0
