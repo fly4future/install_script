2d2d17c98ee62527dd3452b932cfbcd09ff5775a
Vid Smole <smole@fly4future.com>
2026-08-05 11:31:39 +0200

    Change Jetson RTC script to work non-interactively

--- subscripts/3PostInstall/6Setup_RTC_on_Jetson.sh
+++ subscripts/3PostInstall/6Setup_RTC_on_Jetson.sh
@@ -14,9 +14,11 @@ if ! command -v hwclock >/dev/null 2>&1; then
     apt-get update && apt-get install -y util-linux
 fi
 
-# Prompt user to confirm they installed a RTC battery
-echo "Ensure there is a battery for the RTC installed. Press Enter to continue or Ctrl+C to abort."
-read -r
+# In interactive mode we prompt the user to confirm they installed a RTC battery
+if [[ "$NON_INTERACTIVE_MODE" -ne 1 ]]; then
+    echo "Ensure there is a battery for the RTC installed. Press Enter to continue or Ctrl+C to abort."
+    read -r
+fi
 
 echo "Writing system time to RTC..."
 sudo hwclock --systohc
